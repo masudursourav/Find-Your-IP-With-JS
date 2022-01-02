d8e3a039c460a7152f9873eac4242916e35ba2b9
Masudur Rahman Sourav <66933835+masudursourav@users.noreply.github.com>
2022-01-02 23:51:13 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,14 @@
-let url = "https://geo.ipify.org/api/v1?apiKey=at_iICwMs66LaPdvgKPPrvjmD7zSZEEF";
+fetch('https://geo.ipify.org/api/v1?apiKey=at_iICwMs66LaPdvgKPPrvjmD7zSZEEF')
+  .then(response => response.json())
+  .then(data => changeTheDom(data));
 
-var AJAXCall = new XMLHttpRequest();
-AJAXCall.open('GET', url);
-AJAXCall.send();
-AJAXCall.addEventListener('load', function(e) {
-    var data = e.target.response;
-    changeTheDom(data);
-});
-
-function changeTheDom(input) {
-    let apiData = JSON.parse(input);
-    let IP = apiData.ip;
+function changeTheDom(data) {
+    let IP = data.ip;
     let dom1 = document.querySelector('.ip');
     dom1.innerHTML = IP;
-    let ISP = apiData.isp;
+    let ISP = data.isp;
     let dom2 = document.querySelector('.isp');
     dom2.innerHTML = ISP;
     let dom3 = document.querySelector('.location');
-    dom3.innerHTML = apiData.location.city + ' , ' + apiData.location.region + ' , ' + apiData.location.country;
+    dom3.innerHTML = data.location.city + ' , ' + data.location.region + ' , ' + data.location.country;
 }
